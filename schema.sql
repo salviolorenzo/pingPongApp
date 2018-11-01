37e245ef263361eaefc31707ac70d5f644753f7d
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-01 10:17:24 -0400

    adds insert statements

--- schema.sql
+++ schema.sql
@@ -1,1 +1,19 @@
 -- should onlmy have create table statements
+create table players(
+    id serial primary key,
+    name varchar (100)
+
+);
+
+create table games(
+    id serial primary key,
+
+    player1_score integer,
+    player2_score integer,
+
+    winner_id integer references players (id),
+
+    player1_id integer references players (id),
+    player2_id integer references players (id)
+
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,16 @@
 -- contains insert statements
+insert into players
+    (name)
+values
+    ('jeff'),
+    ('broseph'),
+    ('chravis'),
+    ('randy');
+
+insert into games
+    (player1_score, player2_score, player1_id, player2_id)
+values
+    (21, 14, 1, 3),
+    (21, 4, 2, 3),
+    (21, 17, 3, 4),
+    (21, 1, 1, 4);
